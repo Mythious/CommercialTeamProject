70ef713ab04bea7831da4208123fa62dae843a8f
DrunkenHyena <62250113+DrunkenHyena@users.noreply.github.com>
2020-03-22 15:40:53 +0000

    Framework Code Development
    Some structural concepts into possible interactions and use of Unity Events

---
+++ Lamplight/Assets/Olumide_Scripts/Game_World_Logic_Script.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine;
+
+public class Game_World_Logic_Script : MonoBehaviour
+{
+    public UnityEvent endGame;
+    public UnityEvent laternLit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        endGame.AddListener(EndGame);
+        laternLit.AddListener(LaternLit);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void EndGame()
+    {
+
+    }
+
+    void LaternLit()
+    {
+
+    }
+}
---
+++ Lamplight/Assets/Olumide_Scripts/Hunter_Script.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hunter_Script : MonoBehaviour
+{
+    Basic_Movement movement;
+
+    private float HunterSpeedDefault = 5;
+    private bool slowdownStatus = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movement = GetComponent<Basic_Movement>();
+        movement.PlayerMovementSpeed = HunterSpeedDefault;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SlowdownStatusEffect()
+    {
+        if (slowdownStatus == false)
+        {
+            slowdownStatus = true;
+            movement.PlayerMovementSpeed *= 0.65f;
+            Invoke("ReturnToNormalSpeed", 4);
+        }
+    }
+
+    void ReturnToNormalSpeed()
+    {
+        slowdownStatus = false;
+        movement.PlayerMovementSpeed = HunterSpeedDefault;
+    }
+}
