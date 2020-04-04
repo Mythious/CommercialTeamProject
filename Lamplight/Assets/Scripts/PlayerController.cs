bb67054724dcc422984f538740ffd782c05afde4
Efren071 <krauss07@hotmail.co.uk>
2020-04-04 01:12:11 +0100

    Models Added
    Added:
    - Lantern
    - Boxes
    - Glass Flask

---
+++ Lamplight/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 7.0f;
+
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = 10.0f;
+        }
+        else
+        {
+            speed = 7.0f;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        rb.transform.Translate(horizontal * speed * Time.deltaTime, 0f, vertical * speed * Time.deltaTime);
+    }
+
+
+}
